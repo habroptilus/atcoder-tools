a3410c0ecc0cbed7c98cf03831facd0a5dc48300
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-05-22 22:02:40 +0900

    自分で考えた方の実装のバグを潰した

--- ABC/abc097_C.py
+++ ABC/abc097_C.py
@@ -1,12 +1,16 @@
 s = input()
 K = int(input())
+alphabet = list("abcdefghijklmnopqrstuvwxyz")
 result = []
-for i in range(len(s)):
-    for j in range(i + 1, min(i + K + 1, len(s) + 1)):
-        result.append(s[i:j])
 
-result = list(set(result))
+for u in alphabet:
+    for i in range(len(s)):
+        if s[i] == u:
+            for j in range(i + 1, min(i + K + 1, len(s) + 1)):
+                result.append(s[i:j])
+    result = list(set(result))
+    if len(result) > K:
+        break
+
 result.sort()
-# print(result)
-# print(len(result))
 print(result[K - 1])
--- ABC/test.py
+++
@@ -1,9 +0,0 @@
-s = input()
-K = int(input())
-subs = []
-for i in range(0, len(s)):
-    for j in range(i + 1, K + i + 1 if i + K <= len(s) else len(s) + 1):
-        subs.append(s[i:j])
-subs = list(set(subs))
-subs.sort()
-print(subs[K - 1])
