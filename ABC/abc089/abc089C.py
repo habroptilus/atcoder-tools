71b3f1d1881fd9a91d3d72ad32ccd8d3ee08a0e8
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-05-30 13:43:18 +0900

    電波

--- ABC/abc089/abc089C.py
+++ ABC/abc089/abc089C.py
@@ -1,2 +1,22 @@
-a = 1
-b = 3
+from functools import reduce
+from itertools import combinations
+N = int(input())
+S = {"M": set([]), "A": set([]), "R": set([]), "C": set([]), "H": set([])}
+for _ in range(N):
+    i = input()
+    head = i[0]
+    if head == "M":
+        S["M"].add(i)
+    elif head == "A":
+        S["A"].add(i)
+    elif head == "R":
+        S["R"].add(i)
+    elif head == "C":
+        S["C"].add(i)
+    elif head == "H":
+        S["H"].add(i)
+result = [len(S[head]) for head in list("MARCH") if len(S[head]) != 0]
+
+ans = sum([reduce(lambda x, y: x * y, list(combinations(result, 3))[i])
+           for i in range(len(result))])
+print(ans)
