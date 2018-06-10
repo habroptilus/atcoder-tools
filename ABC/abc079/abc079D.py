fd3027e4c9a19fa671afd8413cbeb2d893e63d14
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-06-11 00:08:51 +0900

    他のDとおしてうさばらし

--- ABC/abc079/abc079D.py
+++ ABC/abc079/abc079D.py
@@ -2,23 +2,26 @@ from collections import Counter, deque
 import heapq
 
 
-def Dijkstra(C):
+def Dijkstra(C, s):
     q = []
     inf = float("inf")
     f = [inf] * 11
     f[-1] = 0
-    f[1] = 0
-    heapq.heappush(q, (0, 1))
+    f[s] = 0
+    heapq.heappush(q, (0, s))
     while len(q) != 0:
-        print(q)
+        # print(q)
+        # print(f)
         cost, index = heapq.heappop(q)
+        if index == 1:
+            return f[index]
         if f[index] < cost:
             continue
         for i in range(10):
             if cost + C[index][i] < f[i]:
                 f[i] = cost + C[index][i]
                 heapq.heappush(q, (f[i], i))
-    return f
+    return "hoge"
 
 
 H, W = map(int, input().split())
@@ -34,7 +37,12 @@ c = Counter()
 for i in range(H):
     for j in range(W):
         c[A[i][j]] += 1
+D = []
+for i in range(10):
+    D.append(Dijkstra(C, i))
+D.append(0)
+s = 0
+for i, j in c.items():
+    s += D[i] * j
 
-f = Dijkstra(C)
-
-print(f)
+print(s)
