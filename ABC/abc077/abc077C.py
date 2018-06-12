62c8957dfc64c200aca7178d0f7ce307133b4b99
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-06-12 16:22:08 +0900

    二分探索の方だけかいておいて放置

--- ABC/abc077/abc077C.py
+++ ABC/abc077/abc077C.py
@@ -1,29 +1,53 @@
+"""
+二分探索O(nlogn)でかいてみたらTLEして、通ってる回答みたら線形探索O(n**2)でもとおっててなんでという気持ち
+二分探索で答えがもとまらず無限ループになってしまうケースがあるぽい？
+"""
 N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-C = list(map(int, input().split()))
+
+
+def binary_search(L, target):
+    """
+    >>> binary_search([1,3,5],2)
+    2
+    >>> binary_search([1,3,5],5)
+    0
+    >>> binary_search([1,3,5],9)
+    0
+    >>> binary_search([1,3,3,3,3,5],3)
+    1
+    >>> binary_search([1,3,3,3,3,5],4)
+    1
+    """
+    if L[0] > target:
+        return len(L)
+    if L[-1] <= target:
+        return 0
+    right_i = len(L) - 1
+    left_i = 0
+    while left_i + 1 != right_i:
+        med_i = (right_i + left_i) // 2
+        if L[med_i] < target:
+            left_i = med_i
+        elif L[med_i] > target:
+            right_i = med_i
+        else:
+            while L[med_i + 1] == target:
+                med_i += 1
+            return len(L) - med_i - 1
+    return len(L) - right_i
+
+
+D = []
+for _ in range(3):
+    D.append(list(map(int, input().split())))
+A, B, C = D
 A.sort()
-B.sort()
+A = [-a for a in A]
+A.sort()
 C.sort()
+ans = 0
 
+for b in B:
+    ans += binary_search(A, -b) * binary_search(C, b)
 
-def hoge():
-    count = 0
-    Ai = -1
-    Ci = -1
-    for i in range(N):
-        if Ai < N - 1:
-            while A[Ai + 1] < B[i]:
-                Ai += 1
-                if Ai == N - 1:
-                    break
-        if Ci < N - 1:
-            while C[Ci + 1] <= B[i]:
-                Ci += 1
-                if Ci == N - 1:
-                    break
-        count += (Ai + 1) * (N - (Ci + 1))
-    return count
-
-
-print(hoge())
+print(ans)
