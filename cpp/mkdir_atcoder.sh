9e22257c1db0a1d45fd8ccdf640584186f20918c
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-08-13 20:45:43 +0900

    add cpp dir

---
+++ cpp/mkdir_atcoder.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# $1=cource name(lower case)
+# $2=number
+
+make_files(){
+    dir=`echo $1 | tr '[a-z]' '[A-Z]'`
+    display_num=`printf %03d $2`
+    if [ ! -e $dir ]; then mkdir $dir ; fi
+    cd $dir
+    if [ ! -e $1$display_num ];then
+        mkdir $1$display_num
+        cd $1$display_num
+        touch input.txt
+        if [[ $1 = "abc"  ]] ; then
+            L=(A B C D)
+        elif [[ $1 = "agc" ]]; then
+            L=(A B C D E F)
+        elif [[ $1 = "arc" ]]; then
+            if [[ "$2" -gt 57 ]]; then
+                L=(C D E F)
+            else
+                L=(A B C D)
+            fi
+        fi
+        for var in ${L[@]}
+        do
+            touch "$1$display_num$var.cpp"
+        done
+    fi
+}
+make_files $1 $2
