d7a09293b05d47e696d0cc39520c348f6c389add
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-08-13 23:44:01 +0900

    Bとおた

---
+++ cpp/ABC/abc064/abc064B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+int main(int argc, char const *argv[]) {
+    int N;
+    std::cin >> N;
+    int a[N];
+    for (size_t i = 0; i < N; i++) {
+        std::cin >> a[i];
+    }
+    std::sort(a,a+N);
+    std::cout << a[N-1]-a[0] << '\n';
+    return 0;
+}
