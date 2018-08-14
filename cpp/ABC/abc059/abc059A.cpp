31a5eb3e0c43a022f08b62d3c63d7487f7ed537b
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-08-14 23:28:18 +0900

    これでもとおる

--- cpp/ABC/abc059/abc059A.cpp
+++ cpp/ABC/abc059/abc059A.cpp
@@ -5,12 +5,10 @@
 int main(int argc, char const *argv[]) {
     std::string s1,s2,s3;
     std::cin >> s1 >> s2 >> s3;
-    std::string t1{s1.front()};
-    std::string t2{s2.front()};
-    std::string t3{s3.front()};
-    transform (t1.begin (), t1.end (), t1.begin (), toupper);
-    transform (t2.begin (), t2.end (), t2.begin (), toupper);
-    transform (t3.begin (), t3.end (), t3.begin (), toupper);
-    std::cout << t1+t2+t3 << '\n';
+
+    transform(s1.begin(),s1.end(),s1.begin(),toupper);
+    transform(s2.begin(),s2.end(),s2.begin(),toupper);
+    transform(s3.begin(),s3.end(),s3.begin(),toupper);
+    std::cout << s1.front() << s2.front()<<s3.front()<<'\n';
     return 0;
 }
