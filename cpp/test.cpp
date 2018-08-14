010eebc22da3773debe19fc21e02399f54dfccc3
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-08-14 09:52:46 +0900

    add test

---
+++ cpp/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(int argc, char const *argv[]) {
+    int a[10]={0,0,0,1,1,1,1,1,1,1};
+    std::cout << a[9] << '\n';
+    std::cout << a[10] << '\n';
+    return 0;
+}
