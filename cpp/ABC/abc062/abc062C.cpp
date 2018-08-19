81fcc43a8835e4331321f46c6db8453cafc76362
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-08-19 10:28:17 +0900

    初のc++400AC

---
+++ cpp/ABC/abc062/abc062C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+long get_min(int X,int Y){
+    long S_a,S_b,S_c;
+    long y;
+    long min =1000000000;
+    for (size_t x = 1; x < X; x++) {
+        S_a=x*Y;
+        y=Y/2;
+        S_b=(X-x)*y;
+        S_c=(X-x)*(Y-y);
+        min=std::min(std::max({S_a,S_b,S_c})-std::min({S_a,S_b,S_c}),min);
+    }
+    return min;
+}
+
+
+int main(int argc, char const *argv[]) {
+    int H,W; std::cin >> H >> W;
+
+    if (H%3==0 || W%3==0) {
+        std::cout << 0 << '\n';
+    }else{
+        int ans1=get_min(H,W);
+        int ans2=get_min(W,H);
+        int ans3=W;
+        int ans4=H;
+
+        std::cout << std::min({ans1,ans2,ans3,ans4}) << '\n';
+    }
+
+    return 0;
+}
