6ed541c83792552286a850cb7beb07036fd38f68
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-08-26 15:33:41 +0900

    size_tについてしらべた

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,35 +1,27 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 typedef std::pair<int, int> pair;
 
-void print(std::vector<pair> x){
+void print(std::vector<pair> x) {
     std::cout << "[";
     for (size_t i = 0; i < x.size(); i++) {
-        std::cout << "(" << x[i].first<< ", " <<x[i].second << ")";
-        if (i!=x.size()-1) {
+        std::cout << "(" << x[i].first << ", " << x[i].second << ")";
+        if (i != x.size() - 1) {
             std::cout << ", ";
         }
     }
     std::cout << "]" << '\n';
 }
 
-bool compare_second(pair p1, pair p2){
-    return p1.second < p2.second;
-}
+bool compare_second(pair p1, pair p2) { return p1.second < p2.second; }
 
 int main(int argc, char const *argv[]) {
-    std::vector<pair> v;
-    int a[5]={1,3,10,5,2};
-    int b[5]={2,1,6,4,7};
-    for (size_t i = 0; i < 5; i++) {
-        v.push_back(pair(a[i],b[i]));
+    for (size_t i = 10; i >= 1; i--) {
+        std::cout << i << '\n';
     }
-
-    print(v);
-    //std::sort(v.begin(),v.end(),compare_second);
-    std::sort(v.begin(),v.end(),[](pair p1, pair p2){return p1.second < p2.second;});
-    print(v);
+    size_t h = -1;
+    std::cout << h << '\n';
     return 0;
 }
