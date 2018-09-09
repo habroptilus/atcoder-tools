1758899aed91d52007a7b5413257628a817f488e
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-09-09 23:14:56 +0900

    49abcまで

--- cpp/ABC/abc049/abc049B.cpp
+++ cpp/ABC/abc049/abc049B.cpp
@@ -1,2 +1,13 @@
 #include <bits/stdc++.h>
 typedef long long ll;
+
+int main(int argc, char const *argv[]) {
+    ll H, W;
+    std::cin >> H >> W;
+    std::string S;
+    for (size_t i = 0; i < H; i++) {
+        std::cin >> S;
+        std::cout << S << '\n' << S << '\n';
+    }
+    return 0;
+}
--- cpp/ABC/abc049/abc049C.cpp
+++ cpp/ABC/abc049/abc049C.cpp
@@ -1,2 +1,22 @@
 #include <bits/stdc++.h>
 typedef long long ll;
+int main(int argc, char const *argv[]) {
+    std::string S;
+    std::cin >> S;
+    std::reverse(S.begin(), S.end());
+
+    while (!S.empty()) {
+        if (S.substr(0, 6) == "resare") {
+            S.erase(S.begin(), S.begin() + 6);
+        } else if (S.substr(0, 7) == "remaerd") {
+            S.erase(S.begin(), S.begin() + 7);
+        } else if (S.substr(0, 5) == "maerd" || S.substr(0, 5) == "esare") {
+            S.erase(S.begin(), S.begin() + 5);
+        } else {
+            std::cout << "NO" << '\n';
+            return 0;
+        }
+    }
+    std::cout << "YES" << '\n';
+    return 0;
+}
