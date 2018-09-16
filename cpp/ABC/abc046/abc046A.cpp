95a51905bf817f5ed3d3058308ef50893db7c695
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-09-16 23:32:43 +0900

    Cまで

--- cpp/ABC/abc046/abc046A.cpp
+++ cpp/ABC/abc046/abc046A.cpp
@@ -1,2 +1,13 @@
 #include <bits/stdc++.h>
 typedef long long ll;
+
+int main(int argc, char const *argv[]) {
+    std::set<ll> S;
+    ll a;
+    for (size_t i = 0; i < 3; i++) {
+        std::cin >> a;
+        S.insert(a);
+    }
+    std::cout << S.size() << '\n';
+    return 0;
+}
