a320e8e72e68b6ec874e8e1168c9070a83bec714
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-09-18 23:44:01 +0900

    dpで300点とった

--- cpp/ABC/abc044/abc044C.cpp
+++ cpp/ABC/abc044/abc044C.cpp
@@ -21,6 +21,45 @@ ll solve(ll *x, ll A, ll n) { // bit全探索する.200点取れるけど300点�
     return ans;
 }
 
+ll solve_dp(ll *x, ll A, ll n) {
+    ll sum = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        sum += x[i];
+    }
+    ll DP[n + 1][n + 1][sum + 1];
+    for (size_t j = 0; j < n + 1; j++) {
+        for (size_t k = 0; k < n + 1; k++) {
+            for (size_t s = 0; s < sum + 1; s++) {
+                if (j >= 1 && s < x[j - 1]) {
+                    DP[j][k][s] = DP[j - 1][k][s];
+                } else if (j >= 1 && s >= x[j - 1] && k >= 1) {
+                    DP[j][k][s] =
+                        DP[j - 1][k][s] + DP[j - 1][k - 1][s - x[j - 1]];
+                } else if (s == 0 && j == 0 && k == 0) {
+                    DP[j][k][s] = 1;
+                } else {
+                    DP[j][k][s] = 0;
+                }
+            }
+        }
+    }
+    ll ans = 0;
+    /*
+    for (size_t i = 1; i < n + 1; i++) {
+        for (size_t j = 1; j < sum + 1; j++) {
+            std::cout << DP[n][i][j] << " ";
+        }
+        std::cout << '\n';
+    }*/
+    for (size_t i = 1; i < std::min(sum / A + 1, n + 1); i++) {
+        ans += DP[n][i][i * A];
+        // std::cout << i << " " << i * A << " " << DP[n][i][i * A] << '\n';
+    };
+
+    return ans;
+}
+
 int main(int argc, char const *argv[]) {
     ll N, A;
     std::cin >> N >> A;
@@ -28,6 +67,6 @@ int main(int argc, char const *argv[]) {
     for (size_t i = 0; i < N; i++) {
         std::cin >> x[i];
     }
-    std::cout << solve(x, A, N) << '\n';
+    std::cout << solve_dp(x, A, N) << '\n';
     return 0;
 }
