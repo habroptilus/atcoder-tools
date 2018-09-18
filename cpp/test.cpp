d3a0d7ad03ea3784bd423ed451bbe0e5d7ff972a
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-09-18 23:44:44 +0900

    modify test

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,21 +1,15 @@
 #include <bits/stdc++.h>
 typedef long long ll;
 
-int main(int argc, char const *argv[]) {
-    ll T = 3;
-    for (size_t x = 1; x < 100; x++) {
-        if (std::ceil((double)x / T) != (x + T - 1) / T) {
-            std::cout << std::ceil((double)x / T) << '\n';
-            std::cout << (x + T - 1) / T << '\n';
-        }
-    }
-    ll y = 10000000000000;
-    std::cout << typeid(std::ceil((double)y / T)).name() << '\n';
-    std::cout << std::ceil((double)y / T) << '\n';
-    std::cout << (y + T - 1) / T << '\n';
+void hoge(int a) { a *= 2; }
+void fuga(int &a) { a *= 2; }
 
-    double hoge = 1.0;
-    ll fuga = 2;
-    std::cout << typeid(hoge * fuga).name() << '\n';
+int main(int argc, char const *argv[]) {
+    if (3) {
+        std::cout << 3 << '\n';
+    }
+    if (0) {
+        std::cout << 0 << '\n';
+    }
     return 0;
 }
