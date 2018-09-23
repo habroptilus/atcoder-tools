57f68cd72669ddd110b43e3852f0e39a97d620f5
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-09-23 22:49:04 +0900

    abまでかなしい

---
+++ python/ABC/abc110/abc110B.py
@@ -0,0 +1,11 @@
+N, M, X, Y = map(int, input().split())
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+
+x.append(X)
+y.append(Y)
+
+if max(x) < min(y):
+    print("No War")
+else:
+    print("War")
