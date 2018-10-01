0b2ded1507d377d2d6565e35131a5f2dfc1d9b79
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-10-01 11:14:19 +0900

    c

--- cpp/ABC/abc036/abc036C.cpp
+++ cpp/ABC/abc036/abc036C.cpp
@@ -1,2 +1,24 @@
 #include <bits/stdc++.h>
 typedef long long ll;
+int main(int argc, char const *argv[]) {
+    ll N;
+    std::cin >> N;
+    ll a[N];
+    for (size_t i = 0; i < N; i++) {
+        std::cin >> a[i];
+    }
+    std::map<ll, ll> m{};
+    std::set<ll> st;
+    for (size_t i = 0; i < N; i++) {
+        st.insert(a[i]);
+    }
+    ll count = 0;
+    for (auto x : st) {
+        m[x] = count;
+        count++;
+    }
+    for (size_t i = 0; i < N; i++) {
+        std::cout << m[a[i]] << '\n';
+    }
+    return 0;
+}
