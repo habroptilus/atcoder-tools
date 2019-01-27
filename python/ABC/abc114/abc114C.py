16519036fd437702ae333d99b0c030618f8f797c
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2019-01-27 13:44:45 +0900

    114c修正

--- python/ABC/abc114/abc114C.py
+++ python/ABC/abc114/abc114C.py
@@ -1,21 +1,28 @@
 import itertools
 N = int(input())
-p = 3
 digits = [3, 5, 7]
-A = []
-flag = False
-while True:
-    for tup in itertools.product(digits, repeat=p):
-        a = int("".join(list(map(str, tup))))
-        if a > N:
-            flag = True
+
+
+def get_A_product(digits, N):
+    A = []
+    flag = False
+    p = 3
+    while True:
+        for tup in itertools.product(digits, repeat=p):
+            a = int("".join(list(map(str, tup))))
+            if a > N:
+                flag = True
+                break
+            A.append(a)
+        if flag:
             break
-        A.append(a)
-    if flag:
-        break
-    p += 1
+        p += 1
+    return A
+
+
 count = 0
+A = get_A_product(digits, N)
 for a in A:
-    if "3" in str(a) and "5" in str(a) and "7" in str(a):
+    if all(str(a).count(c) > 0 for c in '753'):
         count += 1
 print(count)
