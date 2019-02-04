7e8779fc7f85026993fc8101cc718efba4a0e7b4
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2019-02-04 12:09:59 +0900

    なおった！

--- python/ABC/abc117/abc117D.py
+++ python/ABC/abc117/abc117D.py
@@ -1,4 +1,3 @@
-# 嘘解法なおそうとおもったけどなおらん。
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
@@ -7,38 +6,34 @@ bin_K = bin(K)
 ma_len = max([len(e) for e in binary] + [len(bin_K)]) - 2
 B = [e[2:].zfill(ma_len) for e in binary]
 bin_K = bin_K[2:].zfill(ma_len)
-ans = 0
-X = []
 
 
-def latter(div, ma_len, B, bin_K):
-    result = 0
-    for i in range(div, ma_len):
-        num_0 = 0
-        num_1 = 0
-        for j in range(len(B)):
-            if B[j][i] == "0":
-                num_0 += 1
-            else:
-                num_1 += 1
-        result += 2**(ma_len - i - 1) * max(num_0, num_1)
-    return result
+def greedy(C):
+    result = []
+    s = 0
+    for i in range(len(C) - 1):
+        s += C[len(C) - i - 1]["score"] * \
+            max(C[len(C) - i - 1]["1"], C[len(C) - i - 1]["0"])
+        result.append(s)
+    return list(reversed(result)) + [0]
 
 
+C = []
+for i in range(ma_len):
+    c = {"0": 0, "1": 0}
+    for j in range(N):
+        c[B[j][i]] += 1
+    c["score"] = 2**(ma_len - i - 1)
+    C.append(c)
+
+latter = greedy(C)
 ans = 0
 former = 0
 for i in range(ma_len):
     if bin_K[i] == "1":
-        med = 2**(ma_len - i - 1) * len([B[j][i]
-                                         for j in range(N) if B[j][i] == "1"])
-        rest = latter(i + 1, ma_len, B, bin_K)
-        ans = max(ans, former + med + rest)
-
-        former += 2**(ma_len - i - 1) * len([B[j][i]
-                                             for j in range(N) if B[j][i] == "0"])
+        ans = max(ans, former + C[i]["1"] * C[i]["score"] + latter[i])
+        former += C[i]["score"] * C[i]["0"]
     else:
-        former += 2**(ma_len - i - 1) * len([B[j][i]
-                                             for j in range(N) if B[j][i] == "1"])
-if ans == 0:
-    ans = former
-print(ans)
+        former += C[i]["score"] * C[i]["1"]
+
+print(max(ans, former))
